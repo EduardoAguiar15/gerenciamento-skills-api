cdf281248aa3b72707ee2b422248c2fe77b0cfad
EduardoAguiar15 <aguiareduardo678@outlook.com>
2025-06-30 15:15:44 -0300

    feat: alteracaoo swagger

--- src/main/java/br/com/neki/config/SwaggerConfig.java
+++ src/main/java/br/com/neki/config/SwaggerConfig.java
@@ -1,26 +1,19 @@
 package br.com.neki.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
 
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("br.com.neki"))
-				.paths(PathSelectors.any()).build().apiInfo(apiInfo());
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("API De Gerenciamento De Usuarios e Skills")
-				.description("API desenvolvida para gerenciar o relacionamento entre usuário e suas skills")
-				.version("1.0.0").build();
-	}
+  @Bean
+  public OpenAPI customOpenAPI() {
+    return new OpenAPI()
+      .info(new Info()
+        .title("API De Gerenciamento De Usuarios e Skills")
+        .version("1.0.0")
+        .description("API desenvolvida para gerenciar o relacionamento entre usuário e suas skills"));
+  }
 }
