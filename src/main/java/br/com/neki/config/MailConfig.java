c159f68ce69cc6e647e213d21db91fae94614a21
Eduardo Aguiar <aguiareduardo678@outlook.com>
2025-06-30 13:52:54 -0300

    Update MailConfig.java

--- src/main/java/br/com/neki/config/MailConfig.java
+++ src/main/java/br/com/neki/config/MailConfig.java
@@ -4,11 +4,11 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
-@Configuration
+@Service
 public class MailConfig {
 
 	@Autowired
